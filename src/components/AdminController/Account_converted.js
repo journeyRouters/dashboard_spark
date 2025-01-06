538e5919e1c950b96efe23e1f00badf37dee7a0b
kishor <backend@journeyrouters.com>
2025-01-06 14:25:13 +0530

    search on destination for converted file pratusha

--- src/components/AdminController/Account_converted.js
+++ src/components/AdminController/Account_converted.js
@@ -1,27 +1,30 @@
-import { collection, getDocs, getFirestore, onSnapshot, orderBy, query, startAt, where } from 'firebase/firestore';
+import { collection, getDocs, getFirestore, onSnapshot, orderBy, query, where } from 'firebase/firestore';
 import moment from 'moment';
 import React, { useEffect, useState } from 'react';
+import '../Accounts/Accounts.css';
 import AccountsMap from '../Accounts/AccountsMap';
-import VouchersCompo from '../payments_vouchers/Vouchers_compo';
 import app from '../required';
-import '../Accounts/Accounts.css'
 const db = getFirestore(app);
 
 
-const Account_converted = ({}) => {
+const Account_converted = ({ }) => {
+    const [Destination, setDestination] = useState('')
     const profile = JSON.parse(localStorage.getItem('profile'));
     const auth = JSON.parse(localStorage.getItem('auth'));
     const [SearchKey, setSearchKey] = useState(0)
     const [input, setInput] = useState('')
-
-    const currentDate = new Date();
     var times = 0
     const [lead_data, set_lead_data] = useState([])
-    const [selectedDate, setDate] = useState(moment(currentDate).format('YYYY-MM-DD'))
-    // function filterOldLeads(leads) {
-    //     const travelinFuture = leads.filter(lead => lead.travelEndDate < currentDate)
+    function HandleDestinationSearchControl() {
+        const q = query(collection(db, "Trip"),
+            where("Lead_Status", "==", "Converted"),
+            where('Destination', '==', Destination),
+            where("quotation_flg", "==", true),
+            orderBy("Travel_Date")
+        );
+        getQueryDatafromDatbase(q)
+    }
 
-    // }
     async function fetchTheSearch() {
         var q;
         switch (SearchKey) {
@@ -140,7 +143,7 @@ const Account_converted = ({}) => {
                     <option value="Contact_Number">Contact Number</option>
                     <option value="Travel_date">Travel Month</option>
                 </select>
-                <input placeholder='search your selection' className={SearchKey == 'Travel_date'?"date":null}
+                <input placeholder='search your selection' className={SearchKey == 'Travel_date' ? "date" : null}
                     onChange={(e) => setInput(e.target.value)}
                 ></input>
                 <input
@@ -148,15 +151,42 @@ const Account_converted = ({}) => {
                     type="button"
                     value="Search "
                     onClick={() => fetchTheSearch()}
-                ></input>
-
-
+                >
+                </input>
+                <div>
+                    <select onChange={(ele) => setDestination(ele.target.value)}>
+                        <option value={'Select Destination'}>change Destination</option>
+                        <option value={'Dubai'}>Dubai</option>
+                        <option value={'Maldives'}>Maldives</option>
+                        <option value={'Thailand'}>Thailand</option>
+                        <option value={'Singapore'}>Singapore</option>
+                        <option value={'Malaysia'}>Malaysia</option>
+                        <option value={'Bali'}>Bali</option>
+                        <option value={'Himachal'}>Himachal</option>
+                        <option value={'Ladakh'}>Ladakh</option>
+                        <option value={'Kerala'}>Kerala</option>
+                        <option value={'Kashmir'}>Kashmir</option>
+                        <option value={'Andaman'}>Andaman</option>
+                        <option value={'Goa'}>Goa</option>
+                        <option value={'Singapore'}>Singapore</option>
+                        <option value={'Rajasthan'}>Rajasthan</option>
+                        <option value={'Veitnam'}>Veitnam</option>
+                        <option value={'Northeast'}>Northeast</option>
+                        <option value={'Europe'}>Europe</option>
+                        <option value={'Turkey'}>Turkey</option>
+                        <option value={'Mauritius'}>Mauritius</option>
+                        <option value={'Baku'}>Baku</option>
+                        <option value={'Almaty'}>Almaty</option>
+                        <option value={'Srilanka'}>Srilanka</option>
+                    </select>
+                    <button onClick={() => HandleDestinationSearchControl()}>Search</button>
+                </div>
             </div>
             {
                 lead_data.map((data, index) => (
-                    
-                        <AccountsMap key={index} data={data} profile={profile} getUpdatedlead={getlead} />
-                    
+
+                    <AccountsMap key={index} data={data} profile={profile} getUpdatedlead={getlead} />
+
                 ))
             }
         </div>
